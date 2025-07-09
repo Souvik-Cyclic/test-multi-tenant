9c4436f04fabce0652048781fd5323115ba8c0a5
Souvik-Cyclic <souvik.cyc@gmail.com>
2025-07-09 17:25:45 +0530

    feat: integrate subdomain middleware

--- subdomain-proxy-backend/src/main.ts
+++ subdomain-proxy-backend/src/main.ts
@@ -4,16 +4,30 @@ import next from 'next';
 import { ExpressAdapter } from '@nestjs/platform-express';
 import express from 'express';
 import * as path from 'path';
+import { SubdomainMiddleware } from './middleware/subdomain.middleware';
+import Redis from 'ioredis';
 
 async function bootstrap() {
   const server = express();
 
+  const redisClient = new Redis({
+    host: 'localhost',
+    port: 6379,
+  });
+
+  const subdomainMiddleware = new SubdomainMiddleware(redisClient);
+
   const frontend = next({
     dev: true,
+    // dev: false,
     dir: path.join(__dirname, '..', 'frontend'),
   });
   const handle = frontend.getRequestHandler();
   await frontend.prepare();
+
+  server.use((req, res, next) => {
+    subdomainMiddleware.use(req, res, next);
+  });
 
   server.use((req, res, next) => {
     return handle(req, res);
